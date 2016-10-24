c1a4a9e21e64793a9203aa9eebd412c8c89e96c0
Sylwekqaz <Sylwekqaz19@gmail.com>
2016-10-25 01:12:23 +0200

    kwantyfikacja theta

--- HoughTestProjects/AcummulatorIndexerTest.cs
+++ HoughTestProjects/AcummulatorIndexerTest.cs
@@ -16,17 +16,23 @@ namespace HoughTestProjects
             var rhoIntervalCount = 4;
 
 
-            var conventer = new AcummulatorIndexConventer(4, 4, rhoIntervalCount, 2);
+            var conventer = new AcummulatorIndexConventer(4, 4, rhoIntervalCount, 4);
 
             var dimensions = conventer.GetAccumulatorDimensions();
 
             Assert.AreEqual(5, dimensions[0]); // jedna extra
+            Assert.AreEqual(4, dimensions[1]);
             // 0     - 22,5
             // 22,5  - 67,5
             // 67,5  - 112,5
             // 112,5 - 157,5
             // 157,5 - 180
-            //todo theta dim
+
+            //theta
+            // -2.828 - -0.0707
+            // -0.707 -  1,141
+            //  1,141 -  3,535
+            //  3.535 -  5.656
         }
 
         [TestMethod]
@@ -36,34 +42,17 @@ namespace HoughTestProjects
             var rhoIntervalCount = 4;
 
 
-            var conventer = new AcummulatorIndexConventer(4, 4, rhoIntervalCount, 2);
+            var conventer = new AcummulatorIndexConventer(4, 4, rhoIntervalCount, 4);
             var pointF = new PolarPointF()
             {
                 Rho = 1.22173048, // 70 degre
-                Theta = 1
+                Theta = 4
             };
 
             var indexes = conventer.GetAccumulatorIndex(pointF);
 
             Assert.AreEqual(2, indexes[0]);
-            // [0] = 0     - 22,5
-            // [1] = 22,5  - 67,5
-            // [2] = 67,5  - 112,5
-            // [3] = 112,5 - 157,5
-            // [4] = 157,5 - 180
-            //todo theta dim
-        }
-
-
-
-        [TestMethod]
-        public void DeserializationTest()
-        {
-            //45 degre 
-            var rhoIntervalCount = 4;
-            var conventer = new AcummulatorIndexConventer(4, 4, rhoIntervalCount, 2);
-
-            List<int> indices = new List<int>() {3,0};
+            Assert.AreEqual(3, indexes[1]);
             //rho
             // [0] = 0     - 22,5
             // [1] = 22,5  - 67,5
@@ -71,12 +60,40 @@ namespace HoughTestProjects
             // [3] = 112,5 - 157,5
             // [4] = 157,5 - 180
 
+            //theta
+            // [0] = -2.828 - -0.0707
+            // [1] = -0.707 -  1,141
+            // [2] =  1,141 -  3,535
+            // [3] =  3.535 -  5.656
+        }
+
+
+        [TestMethod]
+        public void DeserializationTest()
+        {
+            //45 degre 
+            var rhoIntervalCount = 4;
+            var conventer = new AcummulatorIndexConventer(4, 4, rhoIntervalCount, 4);
+
+            List<int> indices = new List<int>() {3, 2};
+            //rho
+            // [0] = 0     - 22,5
+            // [1] = 22,5  - 67,5
+            // [2] = 67,5  - 112,5
+            // [3] = 112,5 - 157,5
+            // [4] = 157,5 - 180
+
+            //theta
+            // [0] = -2.828 - -0.0707
+            // [1] = -0.707 -  1,414
+            // [2] =  1,414 -  3,535
+            // [3] =  3.535 -  5.656
+
             var pointF = conventer.GetLineFromIndex(indices);
 
             //2.35619449 rad =  135 deg - middle of 3rd range
-            Assert.AreEqual(2.35619449, pointF.Rho,0.0001);
-
-            //todo theta dim
+            Assert.AreEqual(2.35619449, pointF.Rho, 0.0001);
+            Assert.AreEqual(2.4745, pointF.Theta, 0.001);
         }
     }
 }
--- hough/AcummulatorIndexConventer.cs
+++ hough/AcummulatorIndexConventer.cs
@@ -6,38 +6,39 @@ namespace Hough
 {
     public class AcummulatorIndexConventer
     {
-        private readonly int _imageWidth;
-        private readonly int _imageHeigth;
+        private readonly double _diagonal;
         private readonly int _rhoIntervalCount;
         private readonly double _thetaDelta;
         private readonly double _rhoDelta;
 
-        public AcummulatorIndexConventer(int imageWidth, int imageHeigth, int rhoIntervalCount, double thetaDelta)
+        public AcummulatorIndexConventer(int imageWidth, int imageHeigth, int rhoIntervalCount, int thetaIntervalCount)
         {
-            _imageWidth = imageWidth;
-            _imageHeigth = imageHeigth;
             _rhoIntervalCount = rhoIntervalCount;
-            _thetaDelta = thetaDelta;
 
-            _rhoDelta =  Math.PI / _rhoIntervalCount;
+            _diagonal = Math.Sqrt(
+                imageHeigth*imageHeigth +
+                imageWidth*imageWidth);
+
+
+            _thetaDelta = 1.5*_diagonal/thetaIntervalCount;
+            _rhoDelta = Math.PI/_rhoIntervalCount;
         }
 
         public List<int> GetAccumulatorDimensions()
         {
             return new List<int>()
             {
-                _rhoIntervalCount +1,
-                //todo theta dim
+                _rhoIntervalCount + 1,
+                (int) Math.Round(_diagonal*1.5/_thetaDelta)
             };
         }
 
         public List<int> GetAccumulatorIndex(PolarPointF pointF)
         {
-            //var 
             return new List<int>()
             {
-                (int) Math.Round(pointF.Rho /_rhoDelta)
-                //todo theta index
+                (int) Math.Round(pointF.Rho/_rhoDelta),
+                (int) ((pointF.Theta + 0.5*_diagonal)/_thetaDelta) // auto math flor
             };
         }
 
@@ -46,7 +47,7 @@ namespace Hough
             return new PolarPointF()
             {
                 Rho = indices[0]*_rhoDelta,
-                Theta = 0 //todo fixme please please please very please, i want be implemented
+                Theta = (-0.5*_diagonal) + (indices[1]*_thetaDelta) + (0.5*_thetaDelta)
             };
         }
     }
