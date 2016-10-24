c195f9878f1b878462249b623fbfe9d352f184e5
folbo <jacunia@op.pl>
2016-10-25 01:13:23 +0200

    Move image processing logic to ImageProcessor class.

---
+++ hough/ImageProcessor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+using Point = System.Drawing.Point;
+
+namespace Hough
+{
+    public class ImageProcessor
+    {
+        private BitmapSource bitmapSource;
+        private byte[] classifiedPixels;
+
+        public ImageProcessor(string path)
+        {
+            bitmapSource = new BitmapImage(new Uri(path));
+            classifiedPixels = BitmapSourceToClassifiedPixels();
+        }
+
+        /// <summary>
+        /// Finds all candidates for Hough's transform.
+        /// </summary>
+        /// <returns>Candidate points.</returns>
+        public List<Point> GetBlackPixels()
+        {
+            int w = bitmapSource.PixelWidth;
+            int h = bitmapSource.PixelHeight;
+
+            List<Point> blackPoints = new List<Point>();
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    var px = classifiedPixels[i * w + j];
+
+                    if (px == 0)
+                        blackPoints.Add(new Point(j, i));
+                }
+            }
+
+            return blackPoints;
+        }
+
+        /// <summary>
+        /// Converts 4 bytes to 1 byte wih classified candidates for hough transform. Candidates have value of 1.
+        /// </summary>
+        /// <returns>Array of pixels. length = width*height</returns>
+        private byte[] BitmapSourceToClassifiedPixels()
+        {
+            int stride = (int)bitmapSource.PixelWidth * (bitmapSource.Format.BitsPerPixel / 8);
+            byte[] pixels = new byte[(int)bitmapSource.PixelHeight * stride];
+
+            bitmapSource.CopyPixels(pixels, stride, 0);
+
+            var result = new byte[bitmapSource.PixelHeight * bitmapSource.PixelWidth];
+
+            // klasyfikacja bgra32 - 1 pixel - 1 bajt.
+            int j = 0;
+            for (int i = 0; i < pixels.Length; i += (bitmapSource.Format.BitsPerPixel / 8))
+            {
+                // dwa srodkowe musza byc czarne xD dla 32bit bgra obrazek
+                if (pixels[i + 1] == 0 && pixels[i + 2] == 0)
+                    result[j] = 0;
+                else
+                    result[j] = 1;
+
+                j++;
+            }
+
+            return result;
+        }
+    }
+}
--- hough/MainWindowVM.cs
+++ hough/MainWindowVM.cs
@@ -25,86 +25,6 @@ namespace Hough
         private RelayCommand _openFileCommand;
         private BitmapSource _bitmapSource;
 
-        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
-        public static extern bool DeleteObject(IntPtr hObject);
-
-        private List<Point> GetBlackPixels(Bitmap bitmap)
-        {
-            int w = bitmap.Width;
-            int h = bitmap.Height;
-
-            IntPtr hbmp = bitmap.GetHbitmap();
-
-            Source = Imaging.CreateBitmapSourceFromHBitmap(
-                hbmp,
-                IntPtr.Zero,
-                Int32Rect.Empty,
-                BitmapSizeOptions.FromEmptyOptions());
-
-            var arr = BitmapSourceToArray(Source);
-
-            List<Point> blackPoints = new List<Point>();
-
-            //byte[] pPixelBuffer = new byte[w*h];
-            //Marshal.Copy(hbmp, pPixelBuffer, 0, 1);
-            /*
-            unsafe
-            {
-                for (int i = 0; i < h; i++)
-                {
-                    byte* pI = (byte*)(void*)hbmp + i * w; //pointer to start of row
-
-                    for (int j = 0; j < w; j++)
-                    {
-                        if (pI[j] == 0x255)
-                        {
-                            blackPoints.Add(new System.Drawing.Point(i, j));
-                        }
-                    }
-                }
-            }*/
-
-            Debug.WriteLine($"{arr.Length}");
-
-            for (int i = 0; i < h; i++)
-            {
-                for (int j = 0; j < w; j++)
-                {
-                    var px = arr[i*w + j];
-                    Debug.WriteLine($"{i * w + j} : {px}");
-                    if (px == 0)
-                    {
-                        blackPoints.Add(new Point(j, i));
-                    }
-                }
-            }
-
-            DeleteObject(hbmp);
-
-            return blackPoints;
-        }
-
-        private byte[] BitmapSourceToArray(BitmapSource bitmapSource)
-        {
-            int stride = (int)bitmapSource.PixelWidth * (bitmapSource.Format.BitsPerPixel / 8);
-            byte[] pixels = new byte[(int)bitmapSource.PixelHeight * stride];
-
-            bitmapSource.CopyPixels(pixels, stride, 0);
-
-            var result = new byte[bitmapSource.PixelHeight * bitmapSource.PixelWidth];
-            int j = 0;
-            for (int i = 0; i < pixels.Length; i += (bitmapSource.Format.BitsPerPixel / 8))
-            {
-                // dwa srodkowe musza byc czarne xD 32bit obrazek
-                if (pixels[i + 1] == 0 && pixels[i + 2] == 0)
-                    result[j] = 0;
-                else result[j] = 1;
-
-                j++;
-            }
-
-            return result;
-        }
 
         public MainWindowVM(IShellService shellService)
         {
@@ -116,13 +36,11 @@ namespace Hough
                 if (string.IsNullOrEmpty(ImagePath))
                     return;
 
-                using (var fs = new FileStream(ImagePath, FileMode.Open))
-                {
-                    var bitmap = new Bitmap(fs);
-                    BlackPixels = GetBlackPixels(bitmap);
-                }
+                ImageProcessor processor = new ImageProcessor(ImagePath);
+                BlackPixels = processor.GetBlackPixels();
             });
         }
+
 
         public string ImagePath
         {
@@ -144,23 +62,33 @@ namespace Hough
             }
         }
 
-        public BitmapSource Source { get { return _bitmapSource; } set { _bitmapSource = value; RaisePropertyChangedEvent(nameof(Source)); } }
+        public BitmapSource Source
+        {
+            get { return _bitmapSource; }
+            set
+            {
+                _bitmapSource = value;
+                RaisePropertyChangedEvent(nameof(Source));
+            }
+        }
 
-
-        public List<Point> BlackPixels { get; set; }
+        public List<Point> BlackPixels
+        {
+            get;
+            set;
+        }
 
         public RelayCommand OpenFileCommand
         {
             get { return _openFileCommand; }
         }
 
-
-
-
         private void GetLines()
         {
             var pairs = BlackPixels.GetCombinationPairs().ToArray();
             
+            //todo zrobic z par linie
+            //
         }
     }
 }
