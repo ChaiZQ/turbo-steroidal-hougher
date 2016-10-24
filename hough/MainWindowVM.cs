25f81ead0fd83e483bedcd16fae9eea938af6847
folbo <jacunia@op.pl>
2016-10-25 00:01:11 +0200

    fix.

--- hough/MainWindowVM.cs
+++ hough/MainWindowVM.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interop;
@@ -13,20 +15,13 @@ using System.Windows.Media.Imaging;
 using Point = System.Drawing.Point;
 
 namespace Hough
-{
-    public struct Pixel
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public byte Val { get; set; }
-    }
-
+{ 
     class MainWindowVM : ViewModel
     {
         private IShellService _shellService;
 
         private string _imagePath;
-        private Pixel[] _imageBytes;
+        private byte[] _imageBytes;
         private RelayCommand _openFileCommand;
 
         [System.Runtime.InteropServices.DllImport("gdi32.dll")]
@@ -38,29 +33,42 @@ namespace Hough
             int h = bitmap.Height;
 
             IntPtr hbmp = bitmap.GetHbitmap();
-
+            /*
             var source = Imaging.CreateBitmapSourceFromHBitmap(
                 hbmp,
                 IntPtr.Zero,
                 Int32Rect.Empty,
                 BitmapSizeOptions.FromEmptyOptions());
-
+                */
             List<Point> blackPoints = new List<Point>();
 
+            //byte[] pPixelBuffer = new byte[w*h];
+            //Marshal.Copy(hbmp, pPixelBuffer, 0, 1);
+            /*
             unsafe
             {
                 for (int i = 0; i < h; i++)
                 {
-                    byte* pI = (byte*)hbmp.ToPointer() + i * h; //pointer to start of row
+                    byte* pI = (byte*)(void*)hbmp + i * w; //pointer to start of row
 
                     for (int j = 0; j < w; j++)
                     {
-                        pI[j] = 0; //pixel
-
                         if (pI[j] == 0x255)
                         {
                             blackPoints.Add(new System.Drawing.Point(i, j));
                         }
+                    }
+                }
+            }*/
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    var px = bitmap.GetPixel(j, i);
+                    if (px.B == 0 && px.G == 0 && px.R == 0)
+                    {
+                        blackPoints.Add(new Point(i, j));
                     }
                 }
             }
@@ -82,10 +90,9 @@ namespace Hough
                     return;
 
                 using (var fs = new FileStream(ImagePath, FileMode.Open))
-                using (var br = new BinaryReader(fs))
                 {
-                    var bitmap = new Bitmap(ImagePath);
-                    var blackPixels = GetBlackPixels(bitmap);
+                    var bitmap = new Bitmap(fs);
+                    BlackPixels = GetBlackPixels(bitmap);
                 }
             });
         }
@@ -100,7 +107,7 @@ namespace Hough
             }
         }
 
-        public Pixel[] ImagePixels
+        public byte[] ImagePixels
         {
             get { return _imageBytes; }
             set
@@ -110,22 +117,20 @@ namespace Hough
             }
         }
 
+        public List<Point> BlackPixels { get; set; }
+
         public RelayCommand OpenFileCommand
         {
             get { return _openFileCommand; }
         }
 
-        
+
 
 
         private void GetLines()
         {
-            var pairs = ImagePixels.GetCombinationPairs().ToArray();
-
-            foreach (var a in pairs)
-            {
-                //a.Item1
-            }
+            var pairs = BlackPixels.GetCombinationPairs().ToArray();
+            
         }
     }
 }
