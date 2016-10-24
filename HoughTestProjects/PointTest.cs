41aaadb83c92907ce9676812e5935419f8941bd4
Sylwekqaz <Sylwekqaz19@gmail.com>
2016-10-24 22:40:01 +0200

    Dodanie modelu punktu

---
+++ HoughTestProjects/PointTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using Hough;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HoughTestProjects
+{
+    [TestClass]
+    public class PointTest
+    {
+        [TestMethod]
+        public void SuccesPath()
+        {
+            var pointF = new PolarPointF(-1,0);
+
+            Assert.AreEqual(pointF.Rho,-1+Math.PI/2, 0.00001);
+
+        }
+
+    }
+}
---
+++ hough/PolarPointF.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Hough
+{
+    public class PolarPointF
+    {
+        private float _rho;
+
+        public PolarPointF(float rho, float theta)
+        {
+            Rho = rho;
+            Theta = theta;
+        }
+
+        /// <summary>
+        ///     value in radians from 0 to pi/2
+        /// </summary>
+        public float Rho
+        {
+            get { return _rho; }
+            set {
+                float m = (float) (Math.PI/2);
+                _rho = (value % m + m) % m; }
+        }
+
+        /// <summary>
+        ///     accept positive and negative values
+        /// </summary>
+        public float Theta { get; set; }
+
+        protected bool Equals(PolarPointF other)
+        {
+            return _rho.Equals(other._rho) && Theta.Equals(other.Theta);
+        }
+
+        public override string ToString()
+        {
+            return $"Rho: {Rho}, Theta: {Theta}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((PolarPointF) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (_rho.GetHashCode()*397) ^ Theta.GetHashCode();
+            }
+        }
+    }
+}
