e54b4fe58c306f7aa332c4f664b0bf9584b7d2a8
Sylwekqaz <Sylwekqaz19@gmail.com>
2016-10-24 21:26:04 +0200

    dodane funkcji linq kombinującej

---
+++ hough/LinqExt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hough
+{
+    public static class LinqExt
+    {
+        public static IEnumerable<Tuple<T, T>> DifferentCombinations<T>(this IEnumerable<T> elements)
+        {
+            return elements.SelectMany((elem, i) => elements.Skip(i + 1).Select(elem2 => new Tuple<T, T>(elem, elem2)));
+        }
+    }
+}
