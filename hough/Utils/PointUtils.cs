4c4f8bceacdd1f893a8259b0b80cea034e74f9ae
Sylwekqaz <Sylwekqaz19@gmail.com>
2016-11-16 13:19:07 +0100

    poprawienie liczenia linii z punktów
    do tej pory liczyliśmy kąt linni, a nie prostopadły do niej

--- hough/Utils/PointUtils.cs
+++ hough/Utils/PointUtils.cs
@@ -9,13 +9,14 @@ namespace Hough.Utils
         {
             var dx = pair.Item1.X - pair.Item2.X;
             var dy = pair.Item1.Y - pair.Item2.Y;
-            var rho = Math.Atan2(dy,dx);
-            var theta = pair.Item1.X*Math.Cos(rho) + pair.Item1.Y*Math.Sin(rho);
-            return new PolarPointF()
+            var rho = Math.Atan2(dy,dx)-(Math.PI/2);
+            var theta = -(pair.Item1.X*Math.Cos(rho) + pair.Item1.Y*Math.Sin(rho));
+            var line = new PolarPointF()
             {
                 Rho = rho,
                 Theta = theta
             };
+            return line;
         }
     }
 }
